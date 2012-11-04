029265751b8a902d8667a39f985008c5e33d5bdc
Jack Danger <jackdanger@squareup.com>
2012-11-03 20:15:10 -0700

    lein new

---
+++ src/eight_puzzle/core.clj
@@ -0,0 +1,1 @@
+(ns eight-puzzle.core)
---
+++ test/eight_puzzle/test/core.clj
@@ -0,0 +1,6 @@
+(ns eight-puzzle.test.core
+  (:use [eight-puzzle.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
