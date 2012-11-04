3ee8b4f7137452a5efa6b9616e77e20c9c4b5453
Jack Danger <jackdanger@squareup.com>
2012-11-04 09:18:49 -0800

    displaying puzzle in lanterna

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject eight-puzzle "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [
-                 [org.clojure/clojure "1.3.0"]
+                 [org.clojure/clojure "1.4.0"]
                  [speclj "2.1.2"]
+                 [clojure-lanterna "0.9.2"]
                  ]
   :plugins [[speclj "2.1.2"]]
   :test-paths ["spec/"]
-  :main eight-puzzle.solver)
+  :gen-class true
+  :main puzzle.solver)
--- src/puzzle/solver.clj
+++ src/puzzle/solver.clj
@@ -1,3 +1,43 @@
-(ns puzzle.solver)
+(ns puzzle.solver
+  (:gen-class)
+  (:require [lanterna.screen :as s]))
 
-(defn -main [& args])
+(def scr (s/get-screen :swing))
+
+(defn put [x y & s#]
+  (s/put-string scr x y (apply str s#)))
+
+(defn redraw []
+  (s/redraw scr))
+
+(defn cell [puzzle n]
+  (let [v (nth puzzle (int n))]
+    (if (= 0 v) " " v)))
+
+(defn row [& parts]
+  (apply str parts))
+
+(defn display [puzzle]
+  (dorun
+    (for [n (range 7)]
+      (if (even? n)
+        ; gap row
+        (put 2 (+ 5 n) " --- --- --- ")
+        ; content row
+        (put 2 (+ 5 n)
+             "| "
+             (cell puzzle (+ 0 (* 3 (Math/floor (/ n 2)))))
+             " | "
+             (cell puzzle (+ 1 (* 3 (Math/floor (/ n 2)))))
+             " | "
+             (cell puzzle (+ 2 (* 3 (Math/floor (/ n 2)))))))))
+             " |"
+  (redraw))
+
+(def p1 [8 3 7 9 1 2 4 0 6])
+
+(defn -main [& args]
+  (println "in main")
+  (s/in-screen scr
+    (display p1)
+    (s/get-key-blocking scr)))
