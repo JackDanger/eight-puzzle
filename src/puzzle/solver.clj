afc6592a329942975eff39ceabd927611f7ec3d1
Jack Danger <jackdanger@squareup.com>
2012-11-06 23:57:31 -0800

    removing unused drawing stuff

--- src/puzzle/solver.clj
+++ src/puzzle/solver.clj
@@ -1,34 +1,4 @@
-(ns puzzle.solver
-  (:gen-class)
-  (:require [clojure.data.priority-map]))
-  ;(:require [lanterna.screen :as s]))
-
-;(def scr (s/get-screen :swing))
-;(defn put [x y & s#]
-  ;(s/put-string scr x y (apply str s#)))
-;(defn redraw []
-  ;(s/redraw scr))
-;(defn display [puzzle]
-;  (dorun
-;    (for [n (range 7)]
-;      (if (even? n)
-;        ; gap row
-;        (put 2 (+ 5 n) " --- --- --- ")
-;        ; content row
-;        (put 2 (+ 5 n)
-;             "| "
-;             (cell puzzle (+ 0 (* 3 (Math/floor (/ n 2)))))
-;             " | "
-;             (cell puzzle (+ 1 (* 3 (Math/floor (/ n 2)))))
-;             " | "
-;             (cell puzzle (+ 2 (* 3 (Math/floor (/ n 2)))))
-;             " |"))))
-;  (redraw))
-;(defn cell [puzzle n]
-;  (let [v (nth puzzle (int n))]
-;    (if (= 0 v) " " v)))
-;(defn row [& parts]
-;  (apply str parts))
+(ns puzzle.solver (:gen-class))
 
 (defn abs [n]
   (if (< 0 n) n (- n)))
