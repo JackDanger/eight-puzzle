7b9a274ec7c80b991cb2b07e203f8efc144d935a
Jack Danger <jackdanger@squareup.com>
2012-11-10 10:33:25 -0800

    starting 15-puzzle work.
    Not optimal. Memory usage is way too high, most paths > 40 steps fail
    hard.

--- src/puzzle/solver.clj
+++ src/puzzle/solver.clj
@@ -1,5 +1,7 @@
 (ns puzzle.solver (:gen-class))
 
+(def size 3); change to 4 to do a 15 puzzle
+
 (defn abs [n]
   (if (< 0 n) n (- n)))
 
@@ -7,7 +9,7 @@
 
 (defn puzzle [p]
   "
-  Takes a vector/list of 9 digits and
+  Takes a vector/list of (size*size)digits and
   returns a map where the keys are the cartesian
   coordinates and the values are the
   corresponding cell contents.
@@ -15,21 +17,21 @@
   (apply assoc
          (cons {}
            (interleave
-             (for [y (range 3)
-                   x (range 3)]
+             (for [y (range size)
+                   x (range size)]
                 [x y])
              p))))
 
 (defn plist [s]
   "
-  Takes a state and returns the 9 digits of the
+  Takes a state and returns the (size*size) digits of the
   puzzle cell values in order.
   "
   (let [p (:puzzle s)]
-    (for [y (range 3)
-          x (range 3)] (p [x y]))))
+    (for [y (range size)
+          x (range size)] (p [x y]))))
 
-(def solution (puzzle (range 9)))
+(def solution (puzzle (range (* size size))))
 (def solution-positions (into {} (map (fn [[k v]] [v k]) solution)))
 
 (defn manhattan-distance [[x1 y1] [x2 y2]]
@@ -55,19 +57,21 @@
   (+ (g state) (h state)))
 
 (def pq-comparator (comparator (fn [a b] (< (a 1) (b 1)))))
-(def frontier (java.util.PriorityQueue. 10000 pq-comparator))
+(def frontier (java.util.PriorityQueue. 1000000 pq-comparator))
 
 (defn branch [state dir]
   (let [puzzle (:puzzle state)
         blank-pos ((first (filter (fn [[pos v]] (zero? v)) puzzle)) 0)
         blankx (blank-pos 0)
         blanky (blank-pos 1)
+        min 0
+        max (dec size)
         [impossible swap]
           (case dir
-            :left  [(= 0 (blank-pos 0)) [(dec blankx) blanky]]
-            :right [(= 2 (blank-pos 0)) [(inc blankx) blanky]]
-            :up    [(= 0 (blank-pos 1)) [blankx (dec blanky)]]
-            :down  [(= 2 (blank-pos 1)) [blankx (inc blanky)]])]
+            :left  [(= min (blank-pos 0)) [(dec blankx) blanky]]
+            :right [(= max (blank-pos 0)) [(inc blankx) blanky]]
+            :up    [(= min (blank-pos 1)) [blankx (dec blanky)]]
+            :down  [(= max (blank-pos 1)) [blankx (inc blanky)]])]
     (if impossible
       nil
       (->State (assoc puzzle
@@ -84,8 +88,9 @@
 (defn search [state visited]
   (let [bs (filter #(not (contains? visited (plist %))) (branches state))]
     ; add all new branches to the frontier
+    ;(if (zero? (mod (.size frontier) 150)) (println (.size frontier) ((.peek frontier) 1)))
     (dorun (for [b bs] (.add frontier [b (f b)])))
-    (if (< 0 (.size frontier))
+    (if (not (zero? (.size frontier)))
       [((.remove frontier) 0)
        (conj visited (plist state))])))
 
@@ -99,19 +104,18 @@
             (recur state# visited#)
             (str "not found in " (count visited) "nodes"))))))
 
-(def p1 (puzzle [5 3 7 8 1 2 4 0 6]))
-(def s1 (->State p1 '()))
+(def p8a  (puzzle [5 3 7 8 1 2 4 0 6]))
+(def p15a (puzzle [5 4 7 8 14 13 12 11 1 2 3 0 10 15 9 6]))
+(def s15 (->State p15a '()))
 
 (defn solveable [n-steps]
   (loop [n n-steps
          s (->State solution '())]
-    (if (zero? n) s
+    (if (zero? n) (->State (:puzzle s) '())
       (let [bs (branches s)
             which (int (Math/floor (* (rand) (count bs))))]
         (recur (dec n)
                (nth bs which))))))
 
 (defn -main [& args]
-  (dorun (for [n (range 10)] (time (println (solve (solveable 1700))))))
-  (time (println (solve (solveable 1700)))))
-  (time (println (solve s1)))
+  (dorun (for [n (range 10)] (time (println (solve (solveable (Integer/parseInt (first args)))))))))
